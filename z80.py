f30b1b3aa474a5442b4199b3e243a2b8ba6e2e95
Carlos Alberto <merecmoney5@gmail.com>
2017-11-19 19:31:13 -0600

    modificado función changeFlag para que funcione correctamente

--- z80.py
+++ z80.py
@@ -22,13 +22,14 @@ class Z80(object):
     @staticmethod
     def hexToBin(numHex):
         return bin(int(numHex, 16))[2:].zfill(8)
-
     @staticmethod
     def binToHex(numBin):
-        return hex(int(numBin, 2))[2:].zfill(8)
+        return hex(int(numBin, 2))[2:].zfill(2)
 
     @staticmethod
     def changeFlag(bitIndex, val):
         Z80.F = Z80.hexToBin(Z80.F)
-        Z80.F = Z80.F[:bitIndex] + val + Z80.F[bitIndex:]
-        return binToHex(Z80.F)
+        Z80.F = Z80.F[:bitIndex] + val + Z80.F[bitIndex+1:]
+        print("banderas "+Z80.F)
+        Z80.F = Z80.binToHex(Z80.F)
+        return Z80.F
