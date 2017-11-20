a166897656e7a6abc9a8249cff6b5d3851458f9a
Carlos Alberto <merecmoney5@gmail.com>
2017-11-19 20:17:17 -0600

    agregado el archivo memoria.py que simula la memoria del Z80

---
+++ memoria.py
@@ -0,0 +1,24 @@
+class Memoria:
+
+	mem = ['--']*65536
+
+	""" Metodo para obtener el contenido de una localidad de memoria
+	dada en HEXADECIMAL """
+	@staticmethod
+	def obtenerContenido(localidad):
+		localidad = int(localidad, 16)
+		contenido = Memoria.mem[localidad]
+		return contenido
+
+	""" Metodo para cambiar el contenido de las localidades de memoria"""
+	@staticmethod
+	def cambiarContenido(contenido, localidad):
+		localidad = int(localidad, 16)
+		Memoria.mem[localidad] = contenido
+		return
+
+	@staticmethod
+	def limpiarMemoria(inicio, fin):
+		for i in range(inicio, fin):
+			Memoria.mem[i] = '--'
+		return
