57116984e65142a48be57279530c630ebe3d593a
Angel Znoker <angel96esti@comunidad.unam.mx>
2017-11-23 10:29:47 -0600

    Terminadas todas las funciones correspondientes, inicio de pruebas

--- testsAngel.py
+++ testsAngel.py
@@ -1,12 +1,3 @@
-#from z80 import Z80
-import funciones
+from z80 import Z80
 
-c = 10
-a = 5
 
-while (a != 10):
-	print(c, a)
-	c -= 1
-	a += 1
-	if c == 0:
-		break
--- z80.py
+++ z80.py
@@ -1,3 +1,4 @@
+import desensamblador
 import funciones
 import memoria
 
@@ -30,6 +31,18 @@ class Z80(object):
     F_ = "00"
     mem = memoria.Memoria()
 
+    # Metodo para mandar llamar a otro de los metodos del procesador mediante el nombre de la operacion a realizar
+    # Si no hay operandos, se manda una cadena vacia
+    @staticmethod
+    def callMethod(object, name, opr):
+        if (opr == ""):
+            getattr(object, name)()
+        elif (len(opr) == 1):
+            getattr(object, name)(opr)
+        else:
+            opr = opr.split(",")
+            getattr(object, name)(opr[0], opr[1])
+
     """ Metodo que cambia las banderas de un bit de terminado
         por un valor dado """
     @staticmethod
@@ -37,6 +50,68 @@ class Z80(object):
         Z80.F = funciones.hexToBin(Z80.F)
         Z80.F = Z80.F[:7 - bitIndex] + val + Z80.F[7 - bitIndex + 1:]
         Z80.F = funciones.binToHex(Z80.F)
+
+    # Funcion para obtener alguna de las banderas
+    @staticmethod
+    def getFlagBit(bitIndex):
+        Z80.F = funciones.hexToBin(Z80.F)
+        estado = int(Z80.F[7 - bitIndex])
+        Z80.F = funciones.binToHex(Z80.F)
+        return estado
+
+    # Mandar en decimal el resultado para checar acarreo
+    @staticmethod
+    def carry(res):
+        res = bin(res)[]2:
+        if (len(res) > 8):
+            Z80.changeFlag(0,'1')
+        else:
+            Z80.changeFlag(0,'0')
+
+    # Mandar el resultado en decimal para checar si es cero
+    @staticmethod
+    def zero(res):
+        if (int(res) == 0):
+            Z80.changeFlag(6,'1')
+        else:
+            Z80.changeFlag(6,'0')
+    
+    # Mandar resultado en decimal para checar paridad
+    @staticmethod
+    def parity(res):
+        res = funciones.tobin(res, 8)
+        cont = res.count('1')
+        if ((cont % 2) == 0):
+            Z80.changeFlag(2,'1')
+        else:
+            Z80.changeFlag(2,'0')
+        
+    # Mandar el resultado en decimal para comprobar sobrepasamiento
+    @staticmethod
+    def overflow(res):
+        if res in range(-128, 128):
+            Z80.changeFlag(2, "0")
+        else:
+            Z80.changeFlag(2, "1")
+            
+    # Mandar resultado en decimal para comprobar el signo:
+    @staticmethod
+    def sign(res):
+        if (res < 0):
+            Z80.changeFlag(7, "1")
+        else:
+            Z80.changeFlag(7, "0")
+    
+    @staticmethod
+    # Mandar ambos operandos en hexadecimal para comprobar medio acarreo y el signo de la operacion a realizar como cadena
+    def halfCarry(op1, op2, o):
+        op1 = str(int(op1[1], 16))
+        op2 = str(int(op2[1], 16))
+        res = eval(op1+o+op2)
+        if res in range(0, 16):
+            Z80.changeFlag(4, "0")
+        else:
+            Z80.changeFlag(4, "1")
 
     """ Metodo que se encarga de obtener el valor hexadecimal de s para las instrucciones que lo ocupan
         s puede ser n, r, (HL), (IX+d) e (IY+d) """
@@ -82,6 +157,22 @@ class Z80(object):
         return cont
 
     @staticmethod
+    def obtenerPosicion(op1):
+        if (op1.find("IX+") != -1 or op1.find("IY+") != -1):
+            op1 = op1.replace("(","").replace(")","").replace("H","").split("+")
+            reg = getattr(Z80, op1[0])
+            des = op1[1]
+            pos = reg + funciones.hextodec(des)
+        # Si es la direccion que contiene HL, se obtiene su contenido
+        elif (op1 == "(HL)" or op1 == "(BC)" or op1 == "(DE)"):
+            pos = int(''.join([getattr(Z80,"H"), getattr(Z80,"L")]), 16)
+        # El ultimo caso es que sea un numero en hexadecimal
+        else:
+            pos = op1.replace('(','').replace(')','').replace('H','')
+            pos = int(pos, 16)
+        return pos
+
+    @staticmethod
     def LD(op1, op2):
         if len(arg1) == 1:
             if op2.find('C)') != -1 or op2.find('E)') != -1 or op2.find('H)') != -1:
@@ -90,8 +181,51 @@ class Z80(object):
             else:
                 s = Z80.obtenerS(op2)
                 setattr(Z80, op1, s)
+                if op2 == 'I' or op2 == 'R':
+                    Z80.zero(int(s))
+                    Z80.changeFlag(2, Z80.IFF1)
+                    Z80.changeFlag(1, '0')
+                    Z80.changeFlag(4, '0')
+
         else:
-            pass
+            if op1.find('(') != -1 and len(op2) == 1:
+                pos = Z80.obtenerPosicion(op1):
+                s = getattr(Z80, op2)
+                Z80.mem.cambiarContenido(s, pos)
+            elif op1.find('(') != -1 and op2.find('H') != 1:
+                pos = Z80.obtenerPosicion(op1)
+                op2 = op2.replace('H','')
+                Z80.mem.cambiarContenido(op2, pos)
+            elif op1.find('('):
+                ss = Z80.obtenerSS(op2)
+                pos = Z80.obtenerPosicion(op1)
+                Z80.mem.cambiarContenido(ss[0], pos + 1)
+                Z80.mem.cambiarContenido(ss[1], pos)
+            elif op1 == 'SP':
+                ss = Z80.obtenerSS(op2)
+                setattr(Z80, op1, ss)
+            else:
+                if op1 == 'IX' or op1 == 'IY' or op1 == 'SP':
+                    if op2.find('('):
+                        pos = Z80.obtenerPosicion(op2)
+                        a1 = Z80.mem.obtenerContenido(pos)
+                        a2 = Z80.mem.obtenerContenido(pos + 1)
+                        setattr(Z80, op1, a2 + a1)
+                    else:
+                        op2 = op2.replace('H', '')
+                        setattr(Z80, op1, op2)
+                else:
+                    if op2.find('('):
+                        pos = Z80.obtenerPosicion(op2)
+                        a1 = Z80.mem.obtenerContenido(pos)
+                        a2 = Z80.mem.obtenerContenido(pos + 1)
+                        setattr(Z80, op1[0], a2)
+                        setattr(Z80, op1[1], a1)
+                    else:
+                        op2 = op2.replace('H', '')
+                        setattr(Z80, op1[0], op2[0])
+                        setattr(Z80, op1[1], op2[1])
+
 
     @staticmethod
     def PUSH(arg):
@@ -192,16 +326,24 @@ class Z80(object):
         Z80.L = dir2[2:]
         # BC <- BC - 1
         dir1 = int(Z80.B + Z80.C, 16) - 1
+        if dir1 == 0:
+            Z80.changeFlag(2, '0')
+        else:
+            Z80.changeFlag(2, '1')
         dir1 = Z80.tohex(dir1, 16)
         Z80.B = dir1[:2]
         Z80.C = dir1[2:]
 
     @staticmethod
     def LDI():
+        Z80.changeFlag(1,'0')
+        Z80.changeFlag(4,'0')
         Z80.transferencia('+')
 
     @staticmethod
     def LDIR():
+        Z80.changeFlag(1,'0')
+        Z80.changeFlag(4,'0')
         c = int(Z80.B + Z80.C, 16)
         while c != 0:
             Z80.transferencia('+')
@@ -209,13 +351,18 @@ class Z80(object):
 
     @staticmethod
     def LDD():
+        Z80.changeFlag(1,'0')
+        Z80.changeFlag(4,'0')
         Z80.transferencia('-')
 
     @staticmethod
     def LDDR():
+        Z80.changeFlag(1,'0')
+        Z80.changeFlag(4,'0')
         c = int(Z80.B + Z80.C, 16)
         while c != 0:
             Z80.transferencia('-')
+            c -= 1
 
     @staticmethod
     def comparacion(sign):
@@ -323,27 +470,53 @@ class Z80(object):
     def ADD(op1, op2):
         if op1 == 'A':
             s = Z80.obtenerS(op2)
-            Z80.A = funciones.tohex(int(Z80.A, 16) + int(s, 16))
+            Z80.halfCarry(Z80.A, s, '+')
+            s = int(Z80.A, 16) + int(s, 16)
+            Z80.carry(s)
+            Z80.zero(s)
+            Z80.overflow(s)
+            Z80.sign(s)
+            Z80.changeFlag(1,'0')
+            Z80.A = funciones.tohex(s)
         elif op1 == 'IX' or op1 == 'IY':
-            s = funciones.tohex(int(getattr(Z80, op1), 16) + int(Z80.obtenerSS(op2), 16), 16)
+            s = int(getattr(Z80, op1), 16) + int(Z80.obtenerSS(op2), 16)
+            Z80.carry(s)
+            Z80.changeFlag(1,'0')
+            s = funciones.tohex(s, 16)
             setattr(Z80, op1, s)
         else:
             HL = ''.join([Z80.H, Z80.L])
             ss = Z80.obtenerSS(op2)
-            HL = funciones.tohex(int(HL, 16) + int(ss, 16))
+            HL = int(HL, 16) + int(ss, 16)
+            Z80.carry(HL)
+            Z80.changeFlag(1,'0')
+            HL = funciones.tohex()
             Z80.H = HL[:2]
             Z80.L = HL[2:]
 
     @staticmethod
-    def SBC(op1, op2):
+    def ADC(op1, op2):
         CY = bin(int(Z80.F,16))[2:].zfill(8)[0]
         if (op1 == "A"):
             s = Z80.obtenerS(op2)
-            Z80.A = funciones.tohex(int(Z80.A, 16) + int(s, 16) + int(CY, 16), 8)
+            Z80.halfCarry(Z80.A, s, '+')
+            s = int(Z80.A, 16) + int(s, 16) + int(CY, 16)
+            Z80.carry(s)
+            Z80.zero(s)
+            Z80.overflow(s)
+            Z80.sign(s)
+            Z80.changeFlag(1,'0')
+            Z80.A = funciones.tohex(s, 8)
         else:
             HL = ''.join([Z80.H, Z80.L])
             ss = Z80.obtenerSS(op2)
-            HL = funciones.tohex(int(HL,16) + int(ss,16) + int(CY, 2), 16)
+            HL = int(HL,16) + int(ss,16) + int(CY, 2)
+            Z80.carry(s)
+            Z80.zero(s)
+            Z80.overflow(s)
+            Z80.sign(s)
+            Z80.changeFlag(1,'0')
+            HL = funciones.tohex(HL, 16)
             # El primer byte se guarda en H y el segundo en L
             Z80.H = HL[:2]
             Z80.L = HL[2:]
@@ -351,4 +524,11 @@ class Z80(object):
     @staticmethod
     def SUB(op1, op2):
         s = Z80.obtenerS(op2)
-        Z80.A = funciones.tohex(int(Z80.A, 16) - int(s, 16))
+        Z80.halfCarry(Z80.A, s, '-')
+        s = int(Z80.A, 16) - int(s, 16)
+        Z80.carry(s)
+        Z80.zero(s)
+        Z80.overflow(s)
+        Z80.sign(s)
+        Z80.changeFlag(1,'1')
+        Z80.A = funciones.tohex(s)
