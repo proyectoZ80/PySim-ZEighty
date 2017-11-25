691f715977f1a0e997b82c6dba1519a04974e920
Angel Znoker <angel96esti@comunidad.unam.mx>
2017-11-25 09:02:14 -0600

    Agregadas las funciones la mayoría de las funciones a Z80, creación del archivo con la función que ejecuta las instrucciones, hacen falta más pruebas

--- memoria.py
+++ memoria.py
@@ -1,7 +1,7 @@
-class Memoria:
-
+class Memoria(object):
+	
 	mem = ['00']*65536
-
+	
 	""" Metodo para obtener el contenido de una localidad de memoria
 	dada en HEXADECIMAL """
 	@staticmethod
@@ -9,16 +9,19 @@ class Memoria:
 		localidad = int(localidad, 16)
 		contenido = Memoria.mem[localidad]
 		return contenido
-
+	
 	""" Metodo para cambiar el contenido de las localidades de memoria"""
 	@staticmethod
 	def cambiarContenido(contenido, localidad):
 		localidad = int(localidad, 16)
 		Memoria.mem[localidad] = contenido
 		return
-
+	
 	@staticmethod
 	def limpiarMemoria(inicio, fin):
 		for i in range(inicio, fin):
-			Memoria.mem[i] = '--'
+			Memoria.mem[i] = '00'
 		return
+		
+				
+		
