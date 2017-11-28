b38a9ed7af643f53730c54966ed9f74d886150dd
Angel Znoker <angel96esti@comunidad.unam.mx>
2017-11-27 23:26:08 -0600

    Arreglados los conflictos del merge del commit anterior

--- Libraries.py
+++ Libraries.py
@@ -1,28 +1,16 @@
-<<<<<<< HEAD
-import z80
-=======
 import Z80
->>>>>>> Angel
 
 class Libraries(object):
 
     @staticmethod
     def ObtainFlag(position):
-<<<<<<< HEAD
-        flags  = z80.Z80.hexToBin(z80.Z80.F)
-=======
         flags  = Z80.Z80.hexToBin(Z80.Z80.F)
->>>>>>> Angel
         return flags[7-position]
 
     @staticmethod
     def checkFlags():
         print("SZXHXPNC")
-<<<<<<< HEAD
-        print(z80.Z80.hexToBin(z80.Z80.F))
-=======
         print(Z80.Z80.hexToBin(Z80.Z80.F))
->>>>>>> Angel
 
     @staticmethod
     def getDisplacement(register):
@@ -34,15 +22,6 @@ class Libraries(object):
         #print("displacement "+ Libraries.getDisplacement(registro))
         if registro == "(HL)":
             #print("es HL")
-<<<<<<< HEAD
-            memoryAddress = getattr(z80.Z80,"H") + getattr(z80.Z80,"L")
-        elif "IX" in registro:
-            #print("es IX")
-            memoryAddress = hex(int(getattr(z80.Z80,"IX"), 16) + int(Libraries.getDisplacement(registro), 16))[2:]
-        else:
-            #print("es IY")
-            memoryAddress = hex(int(getattr(z80.Z80,"IY"), 16) + int(Libraries.getDisplacement(registro), 16))[2:]
-=======
             memoryAddress = getattr(Z80.Z80,"H") + getattr(Z80.Z80,"L")
         elif "IX" in registro:
             #print("es IX")
@@ -50,7 +29,6 @@ class Libraries(object):
         else:
             #print("es IY")
             memoryAddress = hex(int(getattr(Z80.Z80,"IY"), 16) + int(Libraries.getDisplacement(registro), 16))[2:]
->>>>>>> Angel
         #print("memory Address " + memoryAddress )
         return memoryAddress
 
@@ -59,30 +37,18 @@ class Libraries(object):
         """ función para hacer rotacion circular hacia la izquierda y set la bandera C
         con la posición 7 de la representación binario del número en Hexadecimal"""
         #convierte el valor del registro  a binario
-<<<<<<< HEAD
-        numberInBIN = z80.Z80.hexToBin(numberInHEX)
-=======
         numberInBIN = Z80.Z80.hexToBin(numberInHEX)
->>>>>>> Angel
         #tmp toma el valor del bit 7
         tmp = numberInBIN[0]
         #pasar el valor del bit 7 a la posición 0 y dezplazar las demás posiciones
         #excepto la del bit 7
         numberInBIN = numberInBIN[1:] + tmp
-<<<<<<< HEAD
-        z80.Z80.changeFlag(0,tmp)
-=======
         Z80.Z80.changeFlag(0,tmp)
->>>>>>> Angel
         return numberInBIN
 
     @staticmethod
     def LeftRotation(numberInHEX):
-<<<<<<< HEAD
-        numberInBIN = z80.Z80.hexToBin(numberInHEX)
-=======
         numberInBIN = Z80.Z80.hexToBin(numberInHEX)
->>>>>>> Angel
         #print("registro en binario "+numberInBIN)
         tmp = numberInBIN[0]
         #print("numero en binario antes de la rotación " + numberInBIN)
@@ -90,124 +56,74 @@ class Libraries(object):
         numberInBIN = numberInBIN[1:] + Libraries.ObtainFlag(0)
         #print("numero en binario después de la rotación "+numberInBIN)
         #C is data from bit 7 of source register
-<<<<<<< HEAD
-        z80.Z80.changeFlag(0,tmp)
-=======
         Z80.Z80.changeFlag(0,tmp)
->>>>>>> Angel
         return numberInBIN
 
     @staticmethod
     def CircularRightRotation(numberInHEX):
         #convierte el valor del registro  a binario
-<<<<<<< HEAD
-        numberInBIN = z80.Z80.hexToBin(numberInHEX)
-=======
         numberInBIN = Z80.Z80.hexToBin(numberInHEX)
->>>>>>> Angel
         #tmp toma el valor del Bit 0
         tmp = numberInBIN[7]
         #pasar el valor del bit 0 a la posición 7 y desplazar las demás posiciones
         #excepto la del bit 0
         numberInBIN = tmp + numberInBIN[:7]
         #C is data from bit 0 of source register
-<<<<<<< HEAD
-        z80.Z80.changeFlag(0, tmp)
-=======
         Z80.Z80.changeFlag(0, tmp)
->>>>>>> Angel
         return numberInBIN
 
     @staticmethod
     def RightRotation(numberInHEX):
         #convierte el valor del registro  a binario
-<<<<<<< HEAD
-        numberInBIN = z80.Z80.hexToBin(numberInHEX)
-=======
         numberInBIN = Z80.Z80.hexToBin(numberInHEX)
->>>>>>> Angel
         #tmp toma el valor del Bit 0
         tmp = numberInBIN[7]
         #pasar el valor del bit 0 a la bandera C y desplazar las demás posiciones
         #excepto la del bit 0 y poner en el bit 7 el valor previo de C
         numberInBIN = Libraries.ObtainFlag(0) + numberInBIN[:7]
         #C is data from bit 0 of source register
-<<<<<<< HEAD
-        z80.Z80.changeFlag(0, tmp)
-=======
         Z80.Z80.changeFlag(0, tmp)
->>>>>>> Angel
         return numberInBIN
 
     @staticmethod
     def ShiftLeft(numberInHEX):
-<<<<<<< HEAD
-        numberInBIN = z80.Z80.hexToBin(numberInHEX)
-=======
         numberInBIN = Z80.Z80.hexToBin(numberInHEX)
->>>>>>> Angel
         print("number bin "+ numberInBIN)
         tmp = numberInBIN[0]
         print("tmp " + tmp)
         numberInBIN = numberInBIN[1:] + "0"
-<<<<<<< HEAD
-        z80.Z80.changeFlag(0, tmp)
-=======
         Z80.Z80.changeFlag(0, tmp)
->>>>>>> Angel
         return numberInBIN
 
     @staticmethod
     def RightArithmeticShift(numberInHEX):
-<<<<<<< HEAD
-        numberInBIN = z80.Z80.hexToBin(numberInHEX)
-=======
         numberInBIN = Z80.Z80.hexToBin(numberInHEX)
->>>>>>> Angel
         print("number bin " + numberInBIN)
         tmp = numberInBIN[7]
         print("tmp " + tmp)
         numberInBIN = numberInBIN[0] + numberInBIN[:7]
         print("number bin con shift aritmetico " + numberInBIN)
-<<<<<<< HEAD
-        z80.Z80.changeFlag(0, tmp)
-=======
         Z80.Z80.changeFlag(0, tmp)
->>>>>>> Angel
         return numberInBIN
 
     @staticmethod
     def RightLogicalShift(numberInHEX):
-<<<<<<< HEAD
-        numberInBIN = z80.Z80.hexToBin(numberInHEX)
-=======
         numberInBIN = Z80.Z80.hexToBin(numberInHEX)
->>>>>>> Angel
         print("number bin " + numberInBIN)
         tmp = numberInBIN[7]
         print("tmp " + tmp)
         numberInBIN = "0" + numberInBIN[:7]
         print("number bin con shift aritmetico " + numberInBIN)
-<<<<<<< HEAD
-        z80.Z80.changeFlag(0, tmp)
-=======
         Z80.Z80.changeFlag(0, tmp)
->>>>>>> Angel
         return numberInBIN
 
 
     @staticmethod
     def ChangeS(numberInBIN):
         if numberInBIN[0] == "1":
-<<<<<<< HEAD
-            z80.Z80.changeFlag(7, "1")
-        else:
-            z80.Z80.changeFlag(7, "0")
-=======
             Z80.Z80.changeFlag(7, "1")
         else:
             Z80.Z80.changeFlag(7, "0")
->>>>>>> Angel
 
     @staticmethod
     def ChangeP(numberInBIN):
@@ -217,25 +133,13 @@ class Libraries(object):
         #set la bandera P
         #si no reset la bandera P
         if (setP % 2 == 0):
-<<<<<<< HEAD
-            z80.Z80.changeFlag(2, "1")
-        else:
-            z80.Z80.changeFlag(2, "0")
-=======
             Z80.Z80.changeFlag(2, "1")
         else:
             Z80.Z80.changeFlag(2, "0")
->>>>>>> Angel
 
     @staticmethod
     def ChangeZ(numberInBIN):
         if numberInBIN == "00000000" :
-<<<<<<< HEAD
-            z80.Z80.changeFlag(6, "1")
-        else:
-            z80.Z80.changeFlag(6, "0")
-=======
             Z80.Z80.changeFlag(6, "1")
         else:
             Z80.Z80.changeFlag(6, "0")
->>>>>>> Angel
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Z80
+
+### Este proyecto emula el funcionamiento del procesador Z80
+
+Lista de Pendientes
+- [x] Desensamblado
+- [x] Funciones del Z80
+- [x] Emulador de la memoria
+- [ ]  GUI
+- [ ] Obtener 10
+
+![Logo](https://octodex.github.com/images/privateinvestocat.jpg)
